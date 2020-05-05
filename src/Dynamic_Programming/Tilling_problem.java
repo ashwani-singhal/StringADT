facc75fad8674fe5c52ed3d412a7c8f8320e9e93
ashwani-singhal <ashwani16081997@gmail.com>
2020-05-05 20:37:26 +0530

    init commit

--- src/Dynamic_Programming/Tilling_problem.java
+++ src/Dynamic_Programming/Tilling_problem.java
@@ -1,5 +1,36 @@
 package Dynamic_Programming;
+import java.util.Scanner;
+
+/* You r given n*2 grid and n tiles of 2*1 ,in how many ways we can form the grid?? */
 
 public class Tilling_problem {
 
+    // recursive calls are too many in this.
+    /*static int tile(int n)
+    {
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return 2;
+        return tile(n-1) + tile(n-2);
+    }*/
+
+    // recursion with memorisation
+    static int ar[] = new int[10000];
+    static int tile(int n)
+    {
+        if (n == 1 || n == 2)
+            return n;
+        if (ar[n] != 0)
+            return ar[n];
+        ar[n] = tile(n-1) + tile(n-2);
+        return ar[n];
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(tile(n));
+
+    }
 }
---
+++ src/matrixDiagonal.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class matrixDiagonal
+{
+
+    private static void printMatrixDiagonal(int[][] matrix)
+    {
+        int r,c;
+        int rowCount = matrix.length;
+        int colCount = matrix[0].length;
+
+        for (int i = 0; i <rowCount ; i++)
+        {
+            for (r=i, c=0; r >=0 && c<colCount; r--,c++)
+            {
+                System.out.print(matrix[r][c]+" ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i <colCount ; i++)
+        {
+            for (r=rowCount-1, c=i; r >=0 && c<colCount; r--,c++)
+            {
+                System.out.print(matrix[r][c]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number of rows");
+        int row = scan.nextInt();
+        System.out.println("Enter number of columns");
+        int col = scan.nextInt();
+        int matrix[][] = new int[row][col];
+        System.out.println("Enter the elements");
+        for (int i = 0; i <row ; i++)
+        {
+            for (int j = 0; j <col ; j++)
+            {
+                matrix[i][j] = scan.nextInt();
+            }
+        }
+
+        printMatrixDiagonal(matrix);
+    }
+}
